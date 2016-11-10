cb146b4a6ef91e71e7f20b811d6b1a8db1d9e86a
keshu17102013 <manikuttan0380@gmail.com>
2016-11-10 19:53:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,10 +51,6 @@ var articles = {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params. articleName;  
-  res.send(createTemplate(articles[articleName]));
-});
 
 var pool = new Pool(config);
 
