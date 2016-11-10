41f6fa8a5c96f77529e2610c9e5ed6fea2391fbb
keshu17102013 <manikuttan0380@gmail.com>
2016-11-10 19:48:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,37 +46,7 @@ var articles = {
   },
 				      
 };
-function createTemplate(data) {
-var title        = data.title;	
-var heading      = data.heading;	
-var date         = data.date;	
-var content      = data.content;
-var htmlTemplate = `  
-      <html>
- <head>
-   <title> ${title} </title>
-   <meta name = "viewport" content = "width = device-width, initial-scale = 1">
-  <link href="/ui/style.css" rel="stylesheet" />
- </head>
-        <body>
-		  <div class = "container">
-		      <div>
-			  <a href = "/"> Home </a>
-			  </div>
-			  </hr>
-			  <h2> ${heading} </h2>
-			  <div>
-			    ${date}
-			  </div>
-			  <div>
-			       ${content}
-			  </div>
-			  
-		  </div>
-        </body> 		
-</html>`;
-return htmlTemplate;
-}
+
 
 
 app.get('/', function (req, res) {
